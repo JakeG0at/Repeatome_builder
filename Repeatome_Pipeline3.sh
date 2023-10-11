0dd7ad22eaba2b5cbfb43e940eb13832d399c8dc
Jakee <100876907+JakeG0at@users.noreply.github.com>
2023-10-11 09:50:55 -0600

    added quotes to variables to prevent globbing

--- Repeatome_Pipeline3.sh
+++ Repeatome_Pipeline3.sh
@@ -45,7 +45,7 @@ while :; do
     Num_of_runs=$((Num_of_runs + 1))
 
     echo "Fixing the reads..."
-    /home/jlamb1/bin/bbmap/repair.sh in1=$read1 in2=$read2 out1=${Base}_N1_fixed.fastq out2=${Base}_N2_fixed.fastq outs=${Base}_singletons.fastq ow=true threads=$threads
+    /home/jlamb1/bin/bbmap/repair.sh in1="$read1" in2="$read2" out1="${Base}"_N1_fixed.fastq out2="${Base}"_N2_fixed.fastq outs="${Base}"_singletons.fastq ow=true threads=$threads
     echo "Done fixing the reads"
 
     # Update the read1 and read2 variables to point to the fixed files
@@ -54,8 +54,8 @@ while :; do
 
     # Take a random sample of 1 million reads
     echo "Sampling 1 million reads..."
-    seqtk sample -s100 $read1 500000 > sub1.fastq &
-    seqtk sample -s100 $read2 500000 > sub2.fastq &
+    seqtk sample -s100 "$read1" 500000 > sub1.fastq &
+    seqtk sample -s100 "$read2" 500000 > sub2.fastq &
     wait
 
     # Get the headers from the sampled reads
@@ -66,23 +66,23 @@ while :; do
     cat headers1.txt headers2.txt > headers.txt
 
     #remove matching headers from read1 and read2
-    grep -v -F -f headers.txt $read1 > filtered1.fastq &
-    grep -v -F -f headers.txt $read2 > filtered2.fastq &
+    grep -v -F -f headers.txt "$read1" > filtered1.fastq &
+    grep -v -F -f headers.txt "$read2" > filtered2.fastq &
     wait
     #Change file names to original names
-    mv filtered1.fastq $read1
-    mv filtered2.fastq $read2
+    mv filtered1.fastq "$read1"
+    mv filtered2.fastq "$read2"
 
     #Convert to fasta that is merged and interleaved
     echo "Converting to interleaved fasta"
     seqtk mergepe sub1.fastq sub2.fastq > merged.fastq
-    seqtk seq -A merged.fastq > ${Base}_sample.fasta
+    seqtk seq -A merged.fastq > "${Base}"_sample.fasta
 
     # Run repeat explorer
     echo "Running Repeat Explorer..."
     source activate /home/jlamb1/bin/miniconda3/envs/eccsplorer || { echo "Failed to activate conda environment"; exit 1; }
 
-    /home/jlamb1/bin/repex_tarean/seqclust -p -A -v ./re_output -s 1000000 -c $threads -C -tax METAZOA3.0 -D BLASTX_W2 ${Base}_sample.fasta
+    /home/jlamb1/bin/repex_tarean/seqclust -p -A -v ./re_output -s 1000000 -c $threads -C -tax METAZOA3.0 -D BLASTX_W2 "${Base}"_sample.fasta
     # Check if Repeat Explorer was successful
     if [ $? -eq 0 ]; then
         echo "Repeat Explorer was successful"
@@ -103,12 +103,12 @@ while :; do
     echo "There were $high_conf_LTRs high confidence LTRs found"
 
     # Check if num of repeats found is > 0
-    if [ $high_conf_SATs -gt 0 ]; then
+    if [ "$high_conf_SATs" -gt 0 ]; then
         zero_SATS_count=0  # reset count if non-zero found
     else
         zero_SATS_count=$((zero_SATS_count + 1))
     fi
-    if [ $high_conf_LTRs -gt 0 ]; then
+    if [ "$high_conf_LTRs" -gt 0 ]; then
         zero_LTRS_count=0  # reset count if non-zero found
     else
         zero_LTRS_count=$((zero_LTRS_count + 1))
@@ -140,43 +140,43 @@ echo "Done indexing the reference library"
 
 echo "Mapping reads to the library..."
 # Map read1 and read2 to the library
-bwa mem -t $threads2 "$LibraryFile" $read1 > ${Base}_${Num_of_runs}_alignment1.sam &
-bwa mem -t $threads2 "$LibraryFile" $read2 > ${Base}_${Num_of_runs}_alignment2.sam &
+bwa mem -t $threads2 "$LibraryFile" "$read1" > "${Base}"_${Num_of_runs}_alignment1.sam &
+bwa mem -t $threads2 "$LibraryFile" "$read2" > "${Base}"_${Num_of_runs}_alignment2.sam &
 wait
 echo "Done mapping reads to the library"
 
 # Convert SAM to BAM
 echo "Converting SAM to BAM..."
-samtools view -@ $threads2 -S -b ${Base}_${Num_of_runs}_alignment1.sam > ${Base}_${Num_of_runs}_alignment1.bam &
-samtools view -@ $threads2 -S -b ${Base}_${Num_of_runs}_alignment2.sam > ${Base}_${Num_of_runs}_alignment2.bam &
+samtools view -@ $threads2 -S -b "${Base}"_${Num_of_runs}_alignment1.sam > "${Base}"_${Num_of_runs}_alignment1.bam &
+samtools view -@ $threads2 -S -b "${Base}"_${Num_of_runs}_alignment2.sam > "${Base}"_${Num_of_runs}_alignment2.bam &
 wait
 echo "Done converting SAM to BAM"
 
 # Sort the BAM file
 echo "Sorting BAM file..."
-samtools sort -@ $threads2 ${Base}_${Num_of_runs}_alignment1.bam -o ${Base}_${Num_of_runs}_alignment1_sorted.bam &
-samtools sort -@ $threads2 ${Base}_${Num_of_runs}_alignment2.bam -o ${Base}_${Num_of_runs}_alignment2_sorted.bam &
+samtools sort -@ $threads2 "${Base}"_${Num_of_runs}_alignment1.bam -o "${Base}"_${Num_of_runs}_alignment1_sorted.bam &
+samtools sort -@ $threads2 "${Base}"_${Num_of_runs}_alignment2.bam -o "${Base}"_${Num_of_runs}_alignment2_sorted.bam &
 wait
 echo "Done sorting BAM file"
 
 # Filter out mapped reads
 echo "Filtering out mapped reads..."
-samtools view -@ $threads2 -b -F 4 ${Base}_${Num_of_runs}_alignment1_sorted.bam > ${Base}_${Num_of_runs}_mapped_reads1.bam &
-samtools view -@ $threads2 -b -F 4 ${Base}_${Num_of_runs}_alignment2_sorted.bam > ${Base}_${Num_of_runs}_mapped_reads2.bam &
+samtools view -@ $threads2 -b -F 4 "${Base}"_${Num_of_runs}_alignment1_sorted.bam > "${Base}"_${Num_of_runs}_mapped_reads1.bam &
+samtools view -@ $threads2 -b -F 4 "${Base}"_${Num_of_runs}_alignment2_sorted.bam > "${Base}"_${Num_of_runs}_mapped_reads2.bam &
 wait
 echo "Done filtering out mapped reads"
 
 # Get list of mapped read names
 echo "Getting list of mapped read names..."
-samtools view -@ $threads2 ${Base}_${Num_of_runs}_mapped_reads1.bam | cut -f1 > ${Base}_${Num_of_runs}_mapped_read_names1.txt &
-samtools view -@ $threads2 ${Base}_${Num_of_runs}_mapped_reads2.bam | cut -f1 > ${Base}_${Num_of_runs}_mapped_read_names2.txt &
+samtools view -@ $threads2 "${Base}"_${Num_of_runs}_mapped_reads1.bam | cut -f1 > "${Base}"_${Num_of_runs}_mapped_read_names1.txt &
+samtools view -@ $threads2 "${Base}"_${Num_of_runs}_mapped_reads2.bam | cut -f1 > "${Base}"_${Num_of_runs}_mapped_read_names2.txt &
 wait
 echo "Done getting list of mapped read names"
 
 # Use seqtk to filter out mapped reads from the original FASTQ files
 echo "Filtering out mapped reads from the original FASTQ files Read1 and Read2"
-seqtk subseq $read1 ${Base}_${Num_of_runs}_mapped_read_names1.txt > ${Base}_${Num_of_runs}_filtered1.fastq &
-seqtk subseq $read2 ${Base}_${Num_of_runs}_mapped_read_names2.txt > ${Base}_${Num_of_runs}_filtered2.fastq &
+seqtk subseq "$read1" "${Base}"_${Num_of_runs}_mapped_read_names1.txt > "${Base}"_${Num_of_runs}_filtered1.fastq &
+seqtk subseq "$read2" "${Base}"_${Num_of_runs}_mapped_read_names2.txt > "${Base}"_${Num_of_runs}_filtered2.fastq &
 wait
 echo "Done filtering out mapped reads from the original FASTQ files Read1 and Read2"
 
@@ -186,11 +186,11 @@ echo "Done filtering out mapped reads from the original FASTQ files Read1 and Re
     #remove temp files, but add a check to see if they exist first
     echo "Removing temp files"
 
-    rm ${Base}_${Num_of_runs}_mapped_read_names2.txt ${Base}_${Num_of_runs}_mapped_read_names1.txt &
-    rm ${Base}_${Num_of_runs}_mapped_reads2.bam ${Base}_${Num_of_runs}_mapped_reads1.bam &
-    rm ${Base}_${Num_of_runs}_alignment2_sorted.bam ${Base}_${Num_of_runs}_alignment1_sorted.bam &
-    rm ${Base}_${Num_of_runs}_alignment2.bam ${Base}_${Num_of_runs}_alignment1.bam &
-    rm ${Base}_${Num_of_runs}_alignment2.sam ${Base}_${Num_of_runs}_alignment1.sam &
+    rm "${Base}"_${Num_of_runs}_mapped_read_names2.txt "${Base}"_${Num_of_runs}_mapped_read_names1.txt &
+    rm "${Base}"_${Num_of_runs}_mapped_reads2.bam "${Base}"_${Num_of_runs}_mapped_reads1.bam &
+    rm "${Base}"_${Num_of_runs}_alignment2_sorted.bam "${Base}"_${Num_of_runs}_alignment1_sorted.bam &
+    rm "${Base}"_${Num_of_runs}_alignment2.bam "${Base}"_${Num_of_runs}_alignment1.bam &
+    rm "${Base}"_${Num_of_runs}_alignment2.sam "${Base}"_${Num_of_runs}_alignment1.sam &
     wait
     echo "Temp files removed"
     
@@ -198,11 +198,11 @@ echo "Done filtering out mapped reads from the original FASTQ files Read1 and Re
     mv ./re_output "$ProjectDir"/Repeat_explorer_outputs/re_output_${Num_of_runs} 
     # Gzip the files inside the re_output_${Num_of_runs}
     for file in /path/to/files/*; do
-    [[ $file != *.gz ]] && gzip $file
+    [[ $file != *.gz ]] && gzip "$file"
     done
 
     # Move the filtered reads to the original read names
-    mv ${Base}_${Num_of_runs}_filtered1.fastq $read1 &
-    mv ${Base}_${Num_of_runs}_filtered2.fastq $read2 &
+    mv "${Base}"_${Num_of_runs}_filtered1.fastq "$read1" &
+    mv "${Base}"_${Num_of_runs}_filtered2.fastq "$read2" &
     wait
 done
